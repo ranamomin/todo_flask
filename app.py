d3f63a5d6b14b9e72f860b691a3d822bce246d55
ranamomin <ranamomin@gmail.com>
2021-08-13 23:42:05 +0000

    Created ToDo app using Flask and Bootstrap

---
+++ app.py
@@ -0,0 +1,65 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+
+class Todo(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200),nullable=False)
+    desc = db.Column(db.String(500),nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"{self.sno} - {self.title}"
+
+
+@app.route("/", methods=["GET","POST"])
+def hello_world():
+    if request.method == "POST":
+        title = request.form["title"]
+        desc = request.form["desc"]
+
+        todo = Todo(title=title, desc=desc)
+        db.session.add(todo)
+        db.session.commit()
+
+    allTodo = Todo.query.all()
+
+    return render_template("index.html",allTodo=allTodo)
+
+
+@app.route("/update/<int:sno>", methods=["GET","POST"])
+def update(sno):
+
+    if request.method == "POST":
+        title = request.form["title"]
+        desc = request.form["desc"]
+
+        todo = Todo.query.filter_by(sno=sno).first()
+        todo.title = title
+        todo.desc = desc
+        db.session.add(todo)
+        db.session.commit()
+        return redirect("https://8000-blush-quelea-5bvoe9zj.ws-us14.gitpod.io/")
+
+    todo = Todo.query.filter_by(sno=sno).first()
+
+    return render_template("update.html",todo=todo)
+
+@app.route("/delete/<int:sno>")
+def delete(sno):
+    del_Todo = Todo.query.filter_by(sno=sno).first()
+    db.session.delete(del_Todo)
+    db.session.commit()
+    # return redirect("/")
+    return redirect("https://8000-blush-quelea-5bvoe9zj.ws-us14.gitpod.io/")
+
+
+if __name__ == "__main__":
+    app.run(debug=True,port=8000)
